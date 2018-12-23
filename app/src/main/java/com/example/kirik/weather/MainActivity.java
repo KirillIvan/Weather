883292ca5bbcc56a7747a0e48ff2709084f3f501
KirillIvan <p_k.r.ivanov@mpt.ru>
2018-12-23 18:38:12 +0300

    обрабатываем нажатие кнопки на главном экране таким образом, что при нажатии внопки выводится второй экран

--- app/src/main/java/com/example/kirik/weather/MainActivity.java
+++ app/src/main/java/com/example/kirik/weather/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.kirik.weather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+
+        View.OnClickListener onclBtn = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_main2);
+            }
+        };
+
+        btn.setOnClickListener(onclBtn);
     }
 }
